5add55f4f18040b12a1e0333814e533065bfdbbc
advithiya-jain <advithiyajain1@gmail.com>
2023-03-25 02:31:26 +0530

    First implementation of the script
    The script now works, recursing into every directory from the given path and setting the cover art on the parent folder of the cover image as well as all audio files in the folder of the cover art. Currently the files supported are hardcoded in like 39. Supported files are: MP3, FLAC, M4A, OGG Vorbis, WAV, AIFF, WMA, and ALAC

--- micon.sh
+++ micon.sh
@@ -1,59 +1,45 @@
 #!/bin/bash
+
 #store the first arguemnt in a variable for the path
 path=$1
 
 #change directory to the path
 cd "$path" || return
 
-ctrj=0
-ctrp=0
-for i in *.jpg; do
-    ctrj=$ctrj+1
-done
-for i in *.png; do
-    ctrp=$ctrp+1
-done
+############################################################################################################
+#
+# gfind (GNU find): 
+#
+# Find all the cover art files in the directory and subdirectories that match the case-insensitive regex:
+# ".*/cover\.\(jpg\|jpeg\|png\)$" (cover.jpg, cover.jpeg, cover.png) and print the path to the file 
+#
+# Using the find command gives us the full path to the cover art file so it's easier to traverse the 
+# directory tree for changing the cover art for the parent directory and the flac files in the directory.
+#
+############################################################################################################
+# while loop: We then pipe the output of the find command to a while loop that reads the output line by line
+gfind . -type f -iregex '.*/cover\.\(jpg\|jpeg\|png\)$' -print0 | while IFS= read -r -d '' file; do
 
-if [ $ctrj -eq $ctrp ] && [ $ctrp+$ctrj -ne 0 ]; then
-    echo "More than one image type found, please remove the extra cover art and try again"
-    exit 1
-elif [ $ctrj -gt 1 ] && [ $ctrp -gt 1 ]; then
-    echo "More than one cover art found, please remove the extra cover art and try again"
+    # Extract the directory path from the file path and add a trailing slash
+    dirpath=${file%/*}/  
+    # Extract the filename from the path
+    filename=${file##*/}
 
-else    
-    for i in *.flac; do
-        fileicon set "$i" cover.png # set the cover art for the file
+    # Change directory to the originally specified path 
+    # (since we cd later to the directory containing the cover art)
+    cd "$path" || exit 1
+
+    # Set the cover art for the parent directory
+    fileicon set "$dirpath" "$file"
+
+    # Change directory to the directory containing the cover art
+    cd "$dirpath" || exit 1
+
+    # Loop through all the flac files in the directory
+    for i in *.mp3 *.flac *.m4a *.ogg *.wav *.aiff *.wma *.alac; do
+
+        # set the cover art for the file using the cover art filename
+        fileicon set "$i" "$filename"
+
     done
-fi
-
-# If the cover art exists as a jpg file
-#if [ -f cover.jpg ]; then
-#    #loop through all the flac files in the directory
-#    for i in *.flac; do
-#        fileicon set "$i" "$path"/cover.jpg # set the cover art for the file
-#    done
-#    exit 1
-#elif [ -f cover.png ]; then
-#    #loop through all the flac files in the directory
-#    for i in *.flac; do
-#        fileicon set "$i" "$path"/cover.png # set the cover art for the file
-#    done
-#    exit 1
-#else
-#    ctr=0
-#    for i in *.jpg; do
-#        ctr++
-#    done
-#    if [ $ctr -gt 1 ]; then
-#        echo "More than one cover art found, please remove the extra cover art and try again"
-#        exit 1
-#    else    
-#        for i in *.jpg; do
-#            fileicon set "$i" "$path"/"$i" # set the cover art for the file
-#        done
-#        exit 1
-#    fi
-#
-#fi
-#
-#
+done
